c48f5a20a94b62d278774dc52b6b57fde7223906
Shreyas-9900 <PES2UG23CS564@pesu.pes.edu>
2026-01-21 14:10:08 +0530

    Fix flats table and flat creation logic

--- server/flat.py
+++ server/flat.py
@@ -2,13 +2,11 @@
 def create_flat():
     data = request.json
 
-    # 🔑 Generate random token
     token = secrets.token_hex(4).upper()
 
     conn = get_db()
     cur = conn.cursor()
 
-    # Only insert flat_number + token (tenant comes later)
     cur.execute("""
         INSERT INTO flats (flat_number, token, token_used)
         VALUES (?, ?, ?)
